3e297423a9913bb84e59ec604b080d4fc4406d12
Methsaan <methsaan@gmail.com>
2018-05-08 22:16:41 -0400

    init

---
+++ GetUserInputThree.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class GetUserInputThree{
+	public static void main(String[] args) {
+		System.out.print("What is your name ? ");
+		Scanner yourName0 = new Scanner(System.in);
+		String yourName =  
+	}
+}
