a6c1ea07ab99a197802baf257e650c53b84fa158
Methsaan <methsaan@gmail.com>
2018-05-13 09:30:00 -0400

    update

---
+++ usefulprograms/calculator.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class calculator {
+	public static void main(String[] args) {
+		System.out.printf("Welcome to the calculator.\nEnter operator: ");
+		Scanner inp = new Scanner(System.in);
+		String op = inp.nextLine();
+		System.out.printf("Enter firstnum: ");
+		Scanner inp2 = new Scanner(System.in);
+		double x = inp2.nextDouble();
+		System.out.printf("Enter second number: ");
+		Scanner inp3 = new Scanner(System.in);
+		double y = inp3.nextDouble();
+		if (op.equalsIgnoreCase("add")) {
+			System.out.println(x + y);
+		}else if (op.equalsIgnoreCase("sub")) {
+			System.out.println(x - y);
+		}else if (op.equalsIgnoreCase("mul")) {
+			System.out.println(x * y);
+		}else if (op.equalsIgnoreCase("div")) {
+			System.out.println(x / y);
+		}else {
+			System.out.println("Invalid operator");
+		}
+	}
+}
