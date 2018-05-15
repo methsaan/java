fb3f7c646d447d407aad76c91bbf2db1d1ca748d
Methsaan Wick <methsaan@gmail.com>
2018-05-15 07:19:35 -0400

    update

--- usefulprograms/randomChooser.java
+++ usefulprograms/randomChooser.java
@@ -3,17 +3,18 @@ import java.util.*;
 public class randomChooser {
 	public static void main(String[] args){
 		int numberOfItems;
-		System.out.print("Welcome to the random chooser.\nEnter number of items: ");
+		System.out.print("Welcome to the random chooser.\nEnter number of items (max: 5): ");
 		Scanner numOfItms = new Scanner(System.in);
 		numberOfItems = numOfItms.nextInt();
-		ArrayList < String > itemArray = new ArrayList < String >(5);
+		ArrayList < String > itemArray = new ArrayList < String >();
 		for (int x = 0; x > numberOfItems; x++){
 			System.out.println("Enter Item " + x + ": ");
 			Scanner nextItem = new Scanner(System.in);
-			itemArray.add(nextItem.nextLine());
+			itemArray.add(1, nextItem.nextLine());
 		}
 		Random randClass1 = new Random();
 		int randChoice = randClass1.nextInt(numberOfItems);
-		System.out.println("The chosen item is: " + itemArray[randChoice]);
+		System.out.println("The chosen item is: " + itemArray.get(randChoice));
+		
 	}
-} 
+}
