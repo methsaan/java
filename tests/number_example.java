7134e6dd3c60cf743539c942f8ef8bb71d49076f
Methsaan Wick <methsaan@gmail.com>
2018-05-16 07:24:15 -0400

    update

---
+++ tests/number_example.java
@@ -0,0 +1,16 @@
+public class number_example{
+	public static void main(String[] args) {
+		double a = 3.423452678;
+		int b = 5784212;
+		long c = 235857921312344L;
+		float d = 24.235235F;
+		byte e = -46;
+		short f = 423;
+		System.out.println("Double: " + a);
+		System.out.println("Integer: " + b);
+		System.out.println("Long: " + c);
+		System.out.println("Float: " + d);
+		System.out.println("Byte: " + e);
+		System.out.println("Short: " + f);
+	}
+}
