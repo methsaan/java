aa9096001905a59257b0e1c3eec7665c6932ba7a
Methsaan Wick <methsaan@gmail.com>
2018-05-19 18:32:31 -0400

    update

--- usefulprograms/randomChooser.java
+++ usefulprograms/randomChooser.java
@@ -2,18 +2,16 @@ import java.util.*;
 
 public class randomChooser {
 	public static void main(String[] args){
-		int numberOfItems;
-		System.out.print("Welcome to the random chooser.\nEnter number of items (max: 5): ");
-		Scanner numOfItms = new Scanner(System.in);
-		numberOfItems = numOfItms.nextInt();
-		ArrayList<String> itemArray = new ArrayList<String>();
-		for (int x = 0; x > numberOfItems; x++){
-			System.out.println("Enter Item " + x + ": ");
-			Scanner nextItem = new Scanner(System.in);
-			itemArray.add(nextItem.nextLine());
+		System.out.printf("Welcome to the random chooser.\nEnter number of items: ");
+		Scanner scanObj = new Scanner(System.in);
+		int numOfTimes = scanObj.nextInt();
+		String[] listx = new String[scanObj.nextLine()];
+		Random randObj = new Random();
+		for (int x = 0; x <= numOfTimes; x++) {
+			System.out.print("Enter item number " + numOfItems + ": ");
+			listx[x] = scanObj.nextLine();
 		}
-		Random randClass1 = new Random();
-		int randChoice = randClass1.nextInt(numberOfItems);
-		System.out.println("The chosen item is: " + itemArray[randChoice]);
+		String chosenOne = breakfasts[randObj.nextInt(numOfItems-1)];
+		System.out.println("The chosen item is " + chosenOne);
 	}
 }
