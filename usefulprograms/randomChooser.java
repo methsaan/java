eb6d3c85ba595216b6e9f1d096bbb97b5420576f
Methsaan <methsaan@gmail.com>
2018-05-20 16:20:00 -0400

    update

--- usefulprograms/randomChooser.java
+++ usefulprograms/randomChooser.java
@@ -5,10 +5,10 @@ public class randomChooser {
 		System.out.printf("Welcome to the random chooser.\nEnter number of items: ");
 		Scanner scanObj = new Scanner(System.in);
 		int numOfTimes = scanObj.nextInt();
-		String[] listx = new String[numOfTimes];
+		String[] listx = {}
 		Random randObj = new Random();
 		for (int x = 0; x <= numOfTimes; x++) {
-			System.out.print("Enter item number " + numOfItems + ": ");
+			System.out.printf("Enter item number %i\n", numOfTimes);
 			listx[x] = scanObj.nextLine();
 		}
 		String chosenOne = breakfasts[randObj.nextInt(numOfItems-1)];
