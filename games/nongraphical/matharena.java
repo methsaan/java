ee62cd5e1a18b08de906a33fe67dd6fe684b0ab1
Methsaan Wick <methsaan@gmail.com>
2018-05-22 20:58:30 -0400

    update

---
+++ games/nongraphical/matharena.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+import java.util.Random;
+
+public class matharena {
+	public static void main() {
+		matharenaprint m = new matharenaprint();
+		String x = m.input("What is your name: ");
+		System.out.println("Your name is " + x);
+	}
+}
---
+++ games/nongraphical/matharenaprint.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+public class matharenaprint {
+	public static void main(String[] args) {
+	}
+	public String input(String printer){
+                System.out.print(printer);
+                Scanner scanObj = new Scanner(System.in);
+                String inputer = scanObj.nextLine();
+                return inputer;
+        }
+}
