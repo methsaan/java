1720c322f0388fcd8c3080750289eea1f7179f69
Methsaan <methsaan@gmail.com>
2018-05-28 07:57:54 -0400

    update

--- games/nongraphical/matharena.java
+++ games/nongraphical/matharena.java
@@ -2,9 +2,19 @@ import java.util.Scanner;
 import java.util.Random;
 
 public class matharena {
-	public static void main() {
-		matharenaprint m = new matharenaprint();
-		String x = m.input("What is your name: ");
-		System.out.println("Your name is " + x);
+	public static void main(String[] args) {
+		int f = Integer.parseInt(input("Enter a number: "));
+		print(f + 4);
 	}
+	public static void print(Object arg0){
+		System.out.println(arg0);
+	}
+	public static String input(Object arg0){
+		System.out.print(arg0);
+		Scanner scanObj = new Scanner(System.in);
+		String whatTheyEntered = scanObj.nextLine();
+		return whatTheyEntered;
+	}
+	public static int int()
 }
+
