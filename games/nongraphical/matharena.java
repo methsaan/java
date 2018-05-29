13df6db101c539b829ebffc507ca12d5aacf1ea3
Methsaan Wick <methsaan@gmail.com>
2018-05-28 20:54:25 -0400

    update

--- games/nongraphical/matharena.java
+++ games/nongraphical/matharena.java
@@ -3,8 +3,8 @@ import java.util.Random;
 
 public class matharena {
 	public static void main(String[] args) {
-		int f = Int(input("Enter a number: "));
-		print(f + 4);
+		int f = randrange(50, 100);
+		print(f);
 	}
 	public static void print(Object arg0){
 		System.out.println(arg0);
@@ -15,9 +15,21 @@ public class matharena {
 		String whatTheyEntered = scanObj.nextLine();
 		return whatTheyEntered;
 	}
-	public static int Int(WhatToConvert) {
+	public static int Int(String whatToConvert) {
 		int x = Integer.parseInt(whatToConvert);
 		return x;
 	}
+	public static double Float(String whatToConvert){
+		double x = Double.parseDouble(whatToConvert);
+		return x;
+	}
+	public static String str(double whatToConvert){
+		String x = Double.toString(whatToConvert);
+		return x;
+	}
+	public static int randrange(int start, int end){
+		Random randObj = new Random();
+		int x = randObj.nextInt(end) + start;
+		return x;
+	}
 }
-
