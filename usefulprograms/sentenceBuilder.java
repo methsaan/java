896674633f2acd7f1ef4cf8531e68d47fbc233b0
Methsaan Wick <methsaan@gmail.com>
2018-06-03 14:10:25 -0400

    update

--- usefulprograms/sentenceBuilder.java
+++ usefulprograms/sentenceBuilder.java
@@ -5,15 +5,17 @@ public class sentenceBuilder {
 	public static void main(String[] args) {
 		String[] SingularArticles = {"A", "The", "Any", "Every"};
 		String[] SingularArticles2 = {"a", "the", "that", "this"};
+		String[] SingularArticles3 = {"a", "the", "any", "every"};
+		String[] SingularArticles4 = {"A", "The", "That", "This"};
 		String[] CommonAnimateSubject = {"man", "person", "woman", "boy", "girl", "dog", "cat", "monkey", "pig", "wolf"};
 		String[] VerbsAnimatePresent = {"wants", "has", "likes", "throws", "dislikes", "sees", "approaches", "hears", "rejects"};
 		String[] CommonObjects = {"book", "banana", "person", "apple", "pancake", "computer", "animal"};
-		Random rand = new Random();
-		String SA = SingularArticles[rand.nextInt(4)];
-		String SA2 = SingularArticles2[rand.nextInt(4)];
-		String CAS = CommonAnimateSubject[rand.nextInt(10)];
-		String VAP = VerbsAnimatePresent[rand.nextInt(9)];
-		String CO = CommonObjects[rand.nextInt(7)];
-		System.out.println(SA + " " + CAS + " " + VAP + " " + SA2 + " " + CO + ".");
+		Random random = new Random();
+		String[][] SAlist = {SingularArticles, SingularArticles4};
+		String[][] SAlist2 = {SingularArticles3, SingularArticles2};
+		String OneOrTwo1 = SAlist[random.nextInt(1)][];
+		String OneOrTwo2 = SAlist[random.nextInt(1)][];
+		System.out.println(OneOrTwo1 + "\t" + OneOrTwo2);
+		//System.out.println(SA + " " + CAS + " " + VAP + " " + SA2 + " " + CO + ".");
 	}
 }
