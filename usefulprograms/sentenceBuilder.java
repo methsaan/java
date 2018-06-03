91c1e5b34f92c9af4a27e2c9d83d4efb2e0ea2e9
Methsaan <methsaan@gmail.com>
2018-06-02 21:45:30 -0400

    udpate

--- usefulprograms/sentenceBuilder.java
+++ usefulprograms/sentenceBuilder.java
@@ -3,10 +3,14 @@ import java.util.Arrays;
 
 public class sentenceBuilder {
 	public static void main(String[] args) {
-		String[] SingularArticles = new String[4] {"a", "the", "any", "every"}
-		String[] CommonAnimateSubject = new String[10] {"man", "person", "woman", "boy", "girl", "dog", "cat", "monkey", "pig", "wolf"}
-		String[] VerbsAnimatePresent = new String[9] {"wants", "has", "likes", "throws", "dislikes", "sees", "approaches", "hears", "rejects"};
-		String[] CommonObjects = new String[7] {"book", "banana", "person", "apple", "pancake", "computer", "animal"}
+		String[] SingularArticles = new String[4];
+		SingularArticles = {"a", "the", "any", "every"};
+		String[] CommonAnimateSubject = new String[10];
+	       	CommonAnimateSubject = {"man", "person", "woman", "boy", "girl", "dog", "cat", "monkey", "pig", "wolf"};
+		String[] VerbsAnimatePresent = new String[9];
+		VerbsAnimatePresent = {"wants", "has", "likes", "throws", "dislikes", "sees", "approaches", "hears", "rejects"};
+		String[] CommonObjects = new String[7];
+		CommonObjects = {"book", "banana", "person", "apple", "pancake", "computer", "animal"};
 		Random rand = new Random();
 		int randSA = rand.nextInt(4);
 		int randCAS = rand.nextInt(10);
