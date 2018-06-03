af3b5920735018daa6a24d1f0e89b4b27d38b0b7
Methsaan <methsaan@gmail.com>
2018-06-03 09:37:02 -0400

    update

--- usefulprograms/sentenceBuilder.java
+++ usefulprograms/sentenceBuilder.java
@@ -8,15 +8,5 @@ public class sentenceBuilder {
 		String[] VerbsAnimatePresent = new String[9] {"wants", "has", "likes", "throws", "dislikes", "sees", "approaches", "hears", "rejects"};
 		String[] CommonObjects = new String[7] CommonObjects = {"book", "banana", "person", "apple", "pancake", "computer", "animal"};
 		Random rand = new Random();
-		int randSA = rand.nextInt(4);
-		int randCAS = rand.nextInt(10);
-		int randVAP = rand.nextInt(9);
-		int randCO = rand.nextInt(7);
-		String a = SingularArticles[randSA];
-		String b = CommonAnimateSubject[randCAS];
-		String c = VerbsAnimatePresent[randVAP];
-		String d = SingularArticles[randSA];
-		String e = CommonObjects[randCO];
-		System.out.printf("%s%s%s%s%s", a, b, c, d, e);
 	}
 }
