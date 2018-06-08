f68de5d43d24f2b379d4a44c65836ad6cff2f77a
Methsaan <methsaan@gmail.com>
2018-06-08 18:59:26 -0400

    update

--- games/nongraphical/hangman.java
+++ games/nongraphical/hangman.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class hangman {
 	public static void main(String[] args) {
-		String[] words = new String[3];
+		String[] words = new String[9];
 		words = {"fifteen", "numbers", "markets", "garbage", "rejects", "illness", "caption", "bullies", "english"};
 		Random generator = new Random();
 		while (true) {
