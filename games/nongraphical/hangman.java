eeb70084276ff3ba34bd2e5c6fff0a13be05ed67
Methsaan <methsaan@gmail.com>
2018-06-08 19:39:28 -0400

    update

--- games/nongraphical/hangman.java
+++ games/nongraphical/hangman.java
@@ -4,12 +4,28 @@ import java.util.Arrays;
 
 public class hangman {
 	public static void main(String[] args) {
-		String[] words = new String[9];
-		words = {"fifteen", "numbers", "markets", "garbage", "rejects", "illness", "caption", "bullies", "english"};
-		Random generator = new Random();
+		String[] words = {"fifteen", "numbers", "markets", "garbage", "rejects", "illness", "caption", "bullies", "english"};
+		Random random = new Random();
+		char[] userProgress = {'_', '_', '_', '_', '_', '_', '_'};
+		Scanner scanner = new Scanner(System.in);
 		while (true) {
-			int randWord = words[generator.nextInt(9)];
-			System.out.println(randWord);
+			String randWord = words[random.nextInt(9)];
+			System.out.print("Guess a letter: ");
+			String userWord = scanner.next();
+			if (randWord.contains(userWord)) {
+				System.out.println("Yup. Correct");
+			}else {
+				System.out.println("Nop. wrong.");
+			}
+			printarray(userProgress);
 		}
 	}
-} 
+	public static void printarray(char[] array){
+		for (int x = 0; x < array.length; x++) {
+			System.out.print(array[x] + " ");
+			if (x == array.length-1) {
+				System.out.println();
+			}
+		}
+	}
+}
