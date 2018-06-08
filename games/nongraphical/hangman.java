5096499d1b8d5535751f89dae2b2a85321e21b43
Methsaan <methsaan@gmail.com>
2018-06-08 18:54:18 -0400

    update

---
+++ games/nongraphical/hangman.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.util.Random;
+import java.util.Arrays;
+
+public class hangman {
+	public static void main(String[] args) {
+		String[] words = new String[3];
+		words = {"fifteen", "numbers", "markets", "garbage", "rejects", "illness", "caption", "bullies", "english"};
+		Random generator = new Random();
+		while (true) {
+			int randWord = words[generator.nextInt(9)];
+			System.out.println(randWord);
+		}
+	}
+} 
