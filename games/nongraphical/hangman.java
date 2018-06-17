d11a0dc536cc7776db9b738a9d3f3d4ac20174b6
Methsaan <methsaan@gmail.com>
2018-06-17 10:11:11 -0400

    update

--- games/nongraphical/hangman.java
+++ games/nongraphical/hangman.java
@@ -8,17 +8,31 @@ public class hangman {
 		String[] words = {"weights", "numbers", "markets", "marcher", "parting", "winters", "caption", "oranges", "english", "smarten", "puberty", "pythons", "disturb", "farting", "blaming", "transit", "injects", "spoiler", "hexagon", "imports", "exports", "informs", "knights", "gymnast", "foreign", "directs", "arduino", "poverty", "problem", "foaming"};
 		Random random = new Random();
 		String[] userProgress = {"_", "_", "_", "_", "_", "_", "_"};
+		String[] strikes = {"{", ".", ".", ".", ".", ".", ".", ".", ".", ".", ".", "}"}
 		Scanner scanner = new Scanner(System.in);
 		while (true) {
+			int strike = 0;
 			String randWord = words[random.nextInt(30)];
 			while (Arrays.asList(userProgress).contains("_")){
 				System.out.print("Guess a letter: ");
 				String userWord = scanner.nextLine();
 				if (randWord.contains(userWord)) {
-					System.out.println("Yup. Correct");
+					System.out.println("Yup.");
 					userProgress[randWord.indexOf(userWord)] = userWord;
+					if (!userProgress.contains("_")){
+						System.out.println("YOU WIN");
+						System.out.println("Word: " + randWord);
+					}
 				}else {
-					System.out.println("Nop. wrong.");
+					System.out.println("Nope.");
+					strike += 1;
+					strikes[strike] = "#";
+					printarray(strikes);
+					if (strikes == {"{", "#", "#", "#", "#", "#", "#", "#", "#", "#", "#", "}"}){
+						System.out.println("GAME OVER");
+						System.out.println("Word: " + randWord);
+						break;
+					}
 				}
 				printarray(userProgress, "");
 			}
