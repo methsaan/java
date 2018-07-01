1989719b0d7ef2b1e157958ae84e17c69d700db6
Methsaan <methsaan@gmail.com>
2018-07-01 18:02:21 -0400

    update

--- games/nongraphical/tictactoe.java
+++ games/nongraphical/tictactoe.java
@@ -1,20 +1,32 @@
 import java.util.Scanner;
 import java.util.Random;
+import java.lang.String;
 
 public class tictactoe {
 	public static void main(String args[]){
-		printgrid("X", "O", "X", "O", "X", "O", "X", "O", "X");
+		String a=" ", b=" ", c=" ", d=" ", e=" ", f=" ", g=" ", h=" ", i=" ";
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter horizontal");
+		int hori = scan.nextInt();
+		System.out.println("Enter vertical");
+		int vert = scan.nextInt();
+		if ((hori == 1) && (vert == 1)){
+			a = "X";b = " ";c = " ";d = " ";e = " ";f = " ";g = " ";h = " ";i = " ";
+		}else{
+			int fsa = 10;
+		}
+		printgrid(a, b, c, d, e, f, g, h, i);
 	}
-	void prntgrid(tl, tm, tr, ml, mm, mr, bl, bm, br){
+	public static void printgrid(String tl, String tm, String tr, String ml, String mm, String mr, String bl, String bm, String br){
 		System.out.printf("___________________\n");
 		System.out.printf("|     |     |     |\n");
-		System.out.printf("|  %s   |  %s   |  %s   |\n", tl, tm, tr);
+		System.out.printf("|  %s  |  %s  |  %s  |\n", tl, tm, tr);
 		System.out.printf("|_____|_____|_____|\n");
 		System.out.printf("|     |     |     |\n");
-		System.out.printf("|  %s   |  %s   |  %s   |\n", ml, mm, mr);
+		System.out.printf("|  %s  |  %s  |  %s  |\n", ml, mm, mr);
 		System.out.printf("|_____|_____|_____|\n");
 		System.out.printf("|     |     |     |\n");
-		System.out.printf("|  %s   |  %s   |  %s   |\n", bl, bm, br);
+		System.out.printf("|  %s  |  %s  |  %s  |\n", bl, bm, br);
 		System.out.printf("|_____|_____|_____|\n");
 	}
 }
