069409cf23cf0175a76b598e13b645a60f4bf826
Methsaan <methsaan@gmail.com>
2018-07-02 12:55:04 -0400

    update

--- games/nongraphical/hangman3.java
+++ games/nongraphical/hangman3.java
@@ -2,6 +2,7 @@ import java.util.Scanner;
 import java.util.Random;
 import java.util.ArrayList;
 import java.io.IOException;
+import java.util.Arrays;
 
 public class hangman {
 	public static void main(String[] args) {
@@ -12,6 +13,7 @@ public class hangman {
 		    System.out.println("Enter word: ");
 		    randWord = scanno.nextLine();
                     ArrayList<String> strikes = new ArrayList<String>();
+		    ArrayList<String> word = new ArrayList<String>();
                     strikes.add("{");
                     for (int x = 0; x < randWord.length()+3; x++){
                             strikes.add(".");
@@ -22,33 +24,46 @@ public class hangman {
                     for (int x = 0; x < randWord.length(); x++){
                             userProgress.add("_");
                     }
-		    System.out.print("\033[H\033[2J");
-		    while (userProgress.contains("_")){
+		    System.out.print("\033[H\033[2J______________\n");
+		    int loop = 0;
+		    String userWord = "";
+		    boolean in = true;
+		    while (loop < word.size()){
 			    System.out.print("Guess a letter: ");
-			    input = scanno.nextLine();
-			    if (randWord.contains(input)){
-				    for (int x = 0; x < randWord.length(); x++){
-				        userProgress.set(randWord.indexOf(input), input);
-				    }
-				    if (!userProgress.contains("_")){
-					    System.out.println("YOU WIN");
-					    System.out.println("Word: " + randWord);
-				    }
+			    userWord = scanno.nextLine();
+			    String temp = in;
+			    if (userWord == wordArray[loop]){
+				    wordArray.set(loop, true)
 			    }else {
-				    System.out.println("Nope.");
-				    strike += 1;
-				    strikes.set(strike, "#");
-				    printArrayList(strikes, "");
-				    if (strike == randWord.length()+3){
-					    System.out.println("GAME OVER");
-					    System.out.println("Word: " + randWord);
-					    break;
-				    }
+				    
 			    }
-			    printArrayList(userProgress, " ");
 		    }
-		    strike = 0;
-		}
+		   // while (userProgress.contains("_")){
+		//	    System.out.print("Guess a letter: ");
+		//	    input = scanno.nextLine();
+			    //if (randWord.contains(input)){
+				//    for (int x = 0; x < randWord.length(); x++){
+				  //      userProgress.set(randWord.indexOf(input), input);
+				    //}
+				//    if (!userProgress.contains("_")){
+				//	    System.out.println("YOU WIN");
+				//	    System.out.println("Word: " + randWord);
+				  //  }
+			    //}else{
+			//	    System.out.println("Nope.");
+			//	    strike += 1;
+			//	    strikes.set(strike, "#");
+			//	    printArrayList(strikes, "");
+			//	    if (strike == randWord.length()+3){
+			//		    System.out.println("GAME OVER");
+			//		    System.out.println("Word: " + randWord);
+			//		    break;
+			//	    }
+			  //  }
+			    //printArrayList(userProgress, " ");
+		  //  }
+		    //strike = 0;
+		//}
 	}
 	public static void printArrayList(ArrayList<String> arraylistf, String separator){
 		for (int x = 0; x < arraylistf.size(); x++){
