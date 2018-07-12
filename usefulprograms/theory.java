f9ee6d10840fdc68b433d0a93ce29a92ec52d73c
Methsaan <methsaan@gmail.com>
2018-07-12 18:58:05 -0400

    update

--- usefulprograms/theory.java
+++ usefulprograms/theory.java
@@ -5,7 +5,9 @@ import java.lang.String;
 public class theory {
 	public static void main(String[] args){
 		//intervals
-		String[] notes = {"Cb", "C ", "C#", "Db", "D ", "D#", "Eb", "E ", "E#", "Fb", "F ", "F#", "Gb", "G ", "G#", "Ab", "A ", "A#", "Bb", "B ", "B#"};
+		System.out.println("1.");
+		System.out.println("  a) Name the following intervals.");
+		String[] notes = {"Cbb", "C  ", "CX ", "Dbb", "D  ", "DX ", "Ebb", "E  ", "EX ", "Fbb", "F  ", "FX ", "Gbb", "G  ", "GX ", "Abb", "A  ", "AX ", "Bbb", "B  ", "BX "};
 		String inter1 = notes[new Random().nextInt(21)];
 		String inter2 = notes[new Random().nextInt(21)];
 		String inter3 = notes[new Random().nextInt(21)];
@@ -16,17 +18,18 @@ public class theory {
 		String inter8 = notes[new Random().nextInt(21)];
 		String inter9 = notes[new Random().nextInt(21)];
 		String inter10 = notes[new Random().nextInt(21)];
-		String[] sharps = {new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " "};
-		String[] flats = {new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " "};
+		String[] sharps = {new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " "};
+		String[] flats = {new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " "};
 		String[] clefs = {"TREBLE ", "BASE   "};
 		if (new Random().nextBoolean() == true){
-			System.out.println("      " + sharps[1] + sharps[2] + "    " + inter[1] + " |");
-			System.out.println(clefs[new Random().nextInt(2)] + sharps[3] + sharps[4] + "     |");
-			System.out.println("      " + sharps[5] + sharps[6] + "    " + inter[2] + " |");
+			System.out.println("      " + sharps[0] + sharps[1] + "      " + inter1 + "  |  " + inter3 + "  |  " + inter5 + "  |  " + inter7 + "  |  " + inter9 + "  |");
+			System.out.println(clefs[new Random().nextInt(2)] + sharps[2] + sharps[3] + "          | " + "      |       |       |       |");
+			System.out.println("      " + sharps[4] + sharps[5] + "      " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
 		}else {
-			System.out.println("       " + flats[1] + flats[2] + "    " + inter[1] + " |");
-                        System.out.println(clefs[new Random().nextInt(2)] + flats[3] + flats[4] + "     |");
-                        System.out.println("       " + flats[5] + flats[6] + "    " + inter[2] + " |");
+			System.out.println("      " + flats[0] + flats[1] + "      " + inter1 + "  |  " + inter3 + "  |  " + inter5 + "  |  " + inter7 + "  |  " + inter9 + "  |");
+			System.out.println(clefs[new Random().nextInt(2)] + flats[2] + flats[3] + "          | " + "      |       |       |       |");
+			System.out.println("      " + flats[4] + flats[5] + "      " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
 		}
+		//rhythm
 	}
 }
