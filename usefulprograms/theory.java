521769854eb7313c071956f5fc652550aff2b545
Methsaan <methsaan@gmail.com>
2018-07-13 07:04:48 -0400

    update

--- usefulprograms/theory.java
+++ usefulprograms/theory.java
@@ -7,39 +7,41 @@ public class theory {
 		//intervals
 		System.out.println("1.");
 		System.out.println("  a) Name the following intervals.");
-		String[] notes = {"Cbb", "C  ", "CX ", "Dbb", "D  ", "DX ", "Ebb", "E  ", "EX ", "Fbb", "F  ", "FX ", "Gbb", "G  ", "GX ", "Abb", "A  ", "AX ", "Bbb", "B  ", "BX "};
-		String inter1 = notes[new Random().nextInt(21)];
-		String inter2 = notes[new Random().nextInt(21)];
-		String inter3 = notes[new Random().nextInt(21)];
-		String inter4 = notes[new Random().nextInt(21)];
-		String inter5 = notes[new Random().nextInt(21)];
-		String inter6 = notes[new Random().nextInt(21)];
-		String inter7 = notes[new Random().nextInt(21)];
-		String inter8 = notes[new Random().nextInt(21)];
-		String inter9 = notes[new Random().nextInt(21)];
-		String inter10 = notes[new Random().nextInt(21)];
+		String[] enharminics = {"Cbb", "CX ", "Dbb", "DX ", "Ebb", "EX ", "Fbb", "FX ", "Gbb", "GX ", "Abb", "AX ", "Bbb", "BX "};
+		String[] wholes = {"C", "D", "E", "F", "G", "A", "B"}
+		String inter1 = enharmonics[new Random().nextInt(14)];
+		String inter2 = wholes[new Random().nextInt(7)];
+		String inter3 = enharminics[new Random().nextInt(14)];
+		String inter4 = wholes[new Random().nextInt(7)];
+		String inter5 = enharmonics[new Random().nextInt(14)];
+		String inter6 = wholes[new Random().nextInt(7)];
+		String inter7 = enharmonics[new Random().nextInt(14)];
+		String inter8 = wholes[new Random().nextInt(7)];
+		String inter9 = enharmonics[new Random().nextInt(14)];
+		String inter10 = wholes[new Random().nextInt(7)];
 		String[] sharps = {new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " "};
 		String[] flats = {new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " "};
 		String[] clefs = {"TREBLE ", "BASS   "};
+		String clef = clefs[new Random().nextInt(2)];
 		if (new Random().nextBoolean() == true){
 			System.out.println("      " + sharps[0] + sharps[1] + "      " + inter1 + "  |  " + inter3 + "  |  " + inter5 + "  |  " + inter7 + "  |  " + inter9 + "  |");
-			System.out.println(clefs[new Random().nextInt(2)] + sharps[2] + sharps[3] + "          | " + "      |       |       |       |");
+			System.out.println(clef + sharps[2] + sharps[3] + "          | " + "      |       |       |       |");
 			System.out.println("      " + sharps[4] + sharps[5] + "      " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
 		}else {
 			System.out.println("      " + flats[0] + flats[1] + "      " + inter1 + "  |  " + inter3 + "  |  " + inter5 + "  |  " + inter7 + "  |  " + inter9 + "  |");
-			System.out.println(clefs[new Random().nextInt(2)] + flats[2] + flats[3] + "          | " + "      |       |       |       |");
+			System.out.println(clef + flats[2] + flats[3] + "          | " + "      |       |       |       |");
 			System.out.println("      " + flats[4] + flats[5] + "      " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
 		}
-		//System.out.println("  b) Change the upper note of each interval enharmonically. Rename each interval.");
-		//if (new Random().nextBoolean() == true){
-                //	System.out.println("      " + sharps[0] + sharps[1] + "         " + "  |     " +  "     |     " + "     |     " + "     |     " + "     |");
-                //	System.out.println(clefs[new Random().nextInt(2)] + sharps[2] + sharps[3] + "          | " + "      |       |       |       |");
-                //	System.out.println("      " + sharps[4] + sharps[5] + "      " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
-                //}else {
-                //	System.out.println("      " + flats[0] + flats[1] + "      " + inter1 + "  |  " + inter3 + "  |  " + inter5 + "  |  " + inter7 + "  |  " + inter9 + "  |");
-                //	System.out.println(clefs[new Random().nextInt(2)] + flats[2] + flats[3] + "          | " + "      |       |       |       |");
-                //	System.out.println("      " + flats[4] + flats[5] + "      " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
-                //}
+		System.out.println("  b) Change the upper note of each interval enharmonically. Rename each interval.");
+		if (new Random().nextBoolean() == true){
+                	System.out.println("      " + sharps[0] + sharps[1] + "       " + inter1 + "  |  " + inter3 +  "  |  " + inter5 + "  |  " + inter7 + "  |  " + inter9 + "  |");
+                	System.out.println(clef + sharps[2] + sharps[3] + "          | " + "      |       |       |       |");
+                	System.out.println("      " + sharps[4] + sharps[5] + "      " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
+                }else {
+                	System.out.println("      " + flats[0] + flats[1] + "      " + inter1 + "  |  " + inter3 + "  |  " + inter5 + "  |  " + inter7 + "  |  " + inter9 + "  |");
+                	System.out.println(clef + flats[2] + flats[3] + "          | " + "      |       |       |       |");
+                	System.out.println("      " + flats[4] + flats[5] + "      " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
+                }
 		//rhythm rests
 		//scale dictation
 		String[] majscale = {"C ", "Db", "D ", "Eb", "E ", "F ", "Gb", "G ", "Ab", "A ", "Bb", "B "};
