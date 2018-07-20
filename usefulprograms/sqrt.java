d8728e4220588ff3aa468cd306f66230e7248c3d
Methsaan <methsaan@gmail.com>
2018-07-20 07:44:36 -0400

    update

---
+++ usefulprograms/sqrt.java
@@ -0,0 +1,19 @@
+import java.*;
+
+public class sqrt {
+	public static void main(String[] args){
+		double C;
+		double A;
+		double B;
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Enter number: ");
+		C = scan.nextDouble();
+		System.out.println("Enter previous perfect square: ");
+		B = scan.nextDouble();
+		System.out.println("Enter next perfect square: ");
+		A = scan.nextDouble();
+		double sqrt = (1.000/(A-B))*(C-B)+Math.sqrt(B);
+		System.out.println("Square root according to formula: " + sqrt);
+		System.out.println("Square root according to Math.sqrt(): " + Math.sqrt(C));
+	}
+}
