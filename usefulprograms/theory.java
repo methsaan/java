d9ccd55f6f4a3ab1f1336207718c0e019e957168
Methsaan <methsaan@gmail.com>
2018-07-24 15:56:53 -0400

    update

--- usefulprograms/theory.java
+++ usefulprograms/theory.java
@@ -7,18 +7,18 @@ public class theory {
 		//intervals
 		System.out.println("1.");
 		System.out.println("  a) Name the following intervals.");
-		String[] enharmonics = {"Cbb", "CX ", "Dbb", "DX ", "Ebb", "EX ", "Fbb", "FX ", "Gbb", "GX ", "Abb", "AX ", "Bbb", "BX "};
-		String[] wholes = {"C  ", "D  ", "E  ", "F  ", "G  ", "A  ", "B  "};
-		String inter1 = enharmonics[new Random().nextInt(14)];
-		String inter2 = wholes[new Random().nextInt(7)];
-		String inter3 = enharmonics[new Random().nextInt(14)];
-		String inter4 = wholes[new Random().nextInt(7)];
-		String inter5 = enharmonics[new Random().nextInt(14)];
-		String inter6 = wholes[new Random().nextInt(7)];
-		String inter7 = enharmonics[new Random().nextInt(14)];
-		String inter8 = wholes[new Random().nextInt(7)];
-		String inter9 = enharmonics[new Random().nextInt(14)];
-		String inter10 = wholes[new Random().nextInt(7)];
+		String[] enharmonics = {"Cbb", "CX ", "Dbb", "DX ", "Ebb", "EX ", "Fbb", "FX ", "Gbb", "GX ", "Abb", "AX ", "Bbb", "BX ", "Db ", "Eb ", "F# ", "C# ", "D# ", "Gb ", "G# ", "Ab ", "A# ", "Bb "};
+		String[] wholes = {"C  ", "D  ", "E  ", "F  ", "G  ", "A  ", "B  ", "C# ", "D# ", "F# ", "G# ", "A# ", "Db", "Eb", "Gb", "Ab", "Bb"};
+		String inter1 = enharmonics[new Random().nextInt(24)];
+		String inter2 = wholes[new Random().nextInt(17)];
+		String inter3 = enharmonics[new Random().nextInt(24)];
+		String inter4 = wholes[new Random().nextInt(17)];
+		String inter5 = enharmonics[new Random().nextInt(24)];
+		String inter6 = wholes[new Random().nextInt(17)];
+		String inter7 = enharmonics[new Random().nextInt(24)];
+		String inter8 = wholes[new Random().nextInt(17)];
+		String inter9 = enharmonics[new Random().nextInt(24)];
+		String inter10 = wholes[new Random().nextInt(17)];
 		String[] sharps = {new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " ", new Random().nextBoolean() ? "#" : " "};
 		String[] flats = {new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " ", new Random().nextBoolean() ? "b" : " "};
 		String[] clefs = {"TREBLE ", "BASS   "};
@@ -44,8 +44,8 @@ public class theory {
                 }
 		//rhythm rests
 		//scale dictation
-		String[] majscale = {"C ", "Db", "D ", "Eb", "E ", "F ", "Gb", "G ", "Ab", "A ", "Bb", "B "};
-		String[] minscale = {"A ", "Bb", "B ", "C ", "C#", "D ", "Eb", "E ", "F ", "F#", "G ", "G#"};
+		String[] majscale = {"C ", "Db", "D ", "Eb", "E ", "F ", "Gb", "G ", "Ab", "A ", "Bb", "B ", "C# ", "D# ", "F# ", "G#", "A#"};
+		String[] minscale = {"A ", "Bb", "B ", "C ", "C#", "D ", "Eb", "E ", "F ", "F#", "G ", "G#", "Db", "};
 		String[] directions = {"ascending", "descending", "ascending and descending"};
 		String scale = new Random().nextBoolean() ? majscale[new Random().nextInt(12)] + " Major": minscale[new Random().nextInt(12)] + " Minor";
 		System.out.println("3. Write the following scales " + directions[2] + ", using the correct key signature for each. Use whole notes.");
