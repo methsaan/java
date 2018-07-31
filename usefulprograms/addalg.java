be1feba7e4d05b5db6d78a05eca82d70d3731fea
Methsaan <methsaan@gmail.com>
2018-07-31 07:41:21 -0400

    update

--- usefulprograms/addalg.java
+++ usefulprograms/addalg.java
@@ -8,12 +8,18 @@ public class addalg {
 		int f = scan.nextInt();
 		System.out.println("Enter second num: ");
 		int s = scan.nextInt();
-		int rem1 = Integer.parseInt(Integer.toString(Integer.parseInt(Integer.toString(s).substring(1)) + Integer.parseInt(Integer.toString(f).substring(1))));
+		System.out.println("Enter third num: (enter 0 if 2 numbers are enough) ");
+		int t = scan.nextInt();
+		System.out.println("Enter fourth num: (enter 0 if 3 numbers are enough) ");
+		int fo = scan.nextInt();
+		int rem1 = Integer.parseInt(Integer.toString(Integer.parseInt(Integer.toString(s).substring(1)) + Integer.parseInt(Integer.toString(f).substring(1)) + Integer.parseInt(Integer.toString(t).substring(1)) + Integer.parseInt(Integer.toString(fo).substring(1));
 		int rem2 = Integer.parseInt(Integer.toString(rem1).substring(0, 1));
 		System.out.println("   " + rem2);
 		System.out.println("   " + f);
 		System.out.println("   " + s);
+		System.out.println("   " + t);
+		System.out.println("   " + fo);
 		System.out.println(" +____");
-		System.out.println("   " + (f+s));
+		System.out.println("   " + (f+s+t+fo));
 	}
 }
