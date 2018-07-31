c7479d7072f614016c5253ac4536719177a15354
Methsaan <methsaan@gmail.com>
2018-07-30 20:28:59 -0400

    update

--- usefulprograms/addalg.java
+++ usefulprograms/addalg.java
@@ -8,8 +8,9 @@ public class addalg {
 		int f = scan.nextInt();
 		System.out.println("Enter second num: ");
 		int s = scan.nextInt();
-		String remStr = "         ";
-		System.out.println(remStr);
+		int rem1 = Integer.parseInt(Integer.toString(Integer.parseInt(Integer.toString(s).substring(1)) + Integer.parseInt(Integer.toString(f).substring(1))));
+		int rem2 = Integer.parseInt(Integer.toString(rem1).substring(0, 1));
+		System.out.println("   " + rem2);
 		System.out.println("   " + f);
 		System.out.println("   " + s);
 		System.out.println(" +____");
