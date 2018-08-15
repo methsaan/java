4b28c21ccb4732d57406e23dfac280ece912dd9c
Methsaan <methsaan@gmail.com>
2018-08-15 13:47:07 -0400

    update

--- usefulprograms/theory.java
+++ usefulprograms/theory.java
@@ -4,7 +4,6 @@ import java.lang.String;
 
 public class theory {
 	public static void main(String[] args){
-		//intervals
 		System.out.println("1.");
 		System.out.println("  a) Name the following intervals.");
 		String[] enharmonics = {"Cbb", "CX ", "Dbb", "DX ", "Ebb", "EX ", "Fbb", "FX ", "Gbb", "GX ", "Abb", "AX ", "Bbb", "BX ", "Db ", "Eb ", "F# ", "C# ", "D# ", "Gb ", "G# ", "Ab ", "A# ", "Bb "};
@@ -43,7 +42,6 @@ public class theory {
                 	System.out.println("      " + flats[4] + flats[5] + "    | " + inter2 + "  |  " + inter4 + "  |  " + inter6 + "  |  " + inter8 + "  |  " + inter10 + "  |");
                 }
 		//rhythm rests
-		//scale dictation
 		String[] majscale = {"C ", "G ", "D ", "A ", "E ", "B ", "Gb", "Db", "Ab", "Eb", "Bb", "F ", "F#", "Cb", "C#"};
 		String[] minscale = {"A ", "E ", "B ", "F#", "C#", "G#", "Eb", "Bb", "F ", "C ", "G ", "D ", "D#", "Eb", "A#"};
 		String[] directions = {"ascending", "descending", "ascending and descending"};
@@ -59,7 +57,6 @@ public class theory {
 		scale = new Random().nextBoolean() ? majscale[new Random().nextInt(15)] + " Major": minscale[new Random().nextInt(15)] + " Minor";
 		System.out.println("  e) " + scale + " in the " + clefs[new Random().nextInt(2)] + " clef");
 		//transposition
-		//chord dictation
 		String chordClef = clefs[new Random().nextInt(2)];
 		String[] quality = {"minor", "major"};
 		String[] minDegrees = {"tonic triad", "subdominant triad", "dominant triad", "relative major triad", "dominant 7th chord", "parrallel major triad", "leading tone diminished 7th"};
@@ -108,10 +105,8 @@ public class theory {
 		boolean randQuality = new Random().nextBoolean();
 		String majOrMin2 = randQuality ? "major" : "minor";
 		System.out.printf("  f) Write the %s %s scale acending using whole notes. Form triads over %s\n", minRoots[new Random().nextInt(12)], randQuality ? "major" : "minor", randQuality ? "all the notes except for the seventh" : "the first, fourth, fifth, sixth, and eighth note");
-		//chord identification
 		//key and time signature identification
 		//composition
-		//history and music terms
 		System.out.println("9.\n  a)");
 		for (int x = 0; x < 10; x++){
 			String[] types = {"vocab", "time periods", "translations", "b info"};
@@ -125,8 +120,21 @@ public class theory {
 			}else if (randType.equals("vocab")){
 				String[] words = {"polyphonic texture", "invention", "motive", "imitation", "sequence", "concerto grosso", "ritornello", "chamber music", "homophonic texture", "sonata form", "exposition", "development", "recapulation"};
 				System.out.println("Definition of " + words[new Random().nextInt(13)] + ". ________________________________");
-			}else {
-				System.out.println("Coming soon");
+			}else if (randType.equals("translations")){
+				String[] iTerms = {"ad libitum", "alla", "animato", "ben", "all'", "bene", "coll'", "colla", "colle", "col", "con", "con brio", "con espressione", "con fuoco", "con grazia", "con moto", "fp", "fortepiano", "grave", "e", "ed", "loco", "ma", "meno", "meno mosso", "non", "non troppo", "piu mosso", "piu", "poco", "poco a poco", "primo", "prima", "quasi", "secondo", "seconda", "sempre", "senza", "sforzando", "sf", "sfz", "simile"};
+				System.out.println("English translation of: " + iTerms[new Random().nextInt(42)]);
+			}else if (randType.equals("b info")){
+				int randSong = new Random().nextInt(3);
+				if (randSong == 1){
+					String[] info = {"Genre", "Preforming forces", "Texture", "Compositional devices"};
+					System.out.println(info[new Random().nextInt(4)] + " of Invention in C Major.\n________________________________________");
+				}else if (randSong == 2){
+					String[] info = {"Genre", "Key", "Tempo", "Structure", "Preforming forces"};
+					System.out.println(info[new Random().nextInt(4)] + " of Bradenburg Concerto No. 5, first movement\n________________________________________");
+				}else {
+					String[] info = {"Genre", "Preforming forces", "Key", "Tempo", "Form"};
+					System.out.println(info[new Random().nextInt(5)] + " of Eine kleine Nachtmusik, K 525, first movement\n________________________________________");
+				}
 			}
 		}
 		//analysis
