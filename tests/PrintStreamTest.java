ab1bb23b4b69dca6c80d96abfccb4336b4592b8a
Methsaan <methsaan@gmail.com>
2018-09-09 13:06:00 -0400

    update

--- tests/PrintStreamTest.java
+++ tests/PrintStreamTest.java
@@ -1,7 +1,5 @@
-import java.*;
-
 public class PrintStreamTest{
 	public static void main(String[] args){
-		System.out.println("Hello");
+		java.lang.System.out.println("Hello world\n\uD7F7");
 	}
 }
