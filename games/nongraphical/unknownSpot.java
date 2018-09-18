e5b9808c03bf9d48de2edfb4f960e508f8322855
Methsaan <methsaan@gmail.com>
2018-09-17 20:20:35 -0400

    update

--- games/nongraphical/unknownSpot.java
+++ games/nongraphical/unknownSpot.java
@@ -1,11 +1,10 @@
-<<<<<<< HEAD
 import java.util.*;
 
 public class unknownSpot {
 	public static void main(String[] args){
 		int points = 0;
 		System.out.println("Welcome to Unknown Spot");
-		for (int x = 0; x < 16; x++) {
+		for (int x = 0; x < 17; x++) {
 			String[] choices = new String[] {"a", "b", "c", "d"};
 			Random randObj = new Random();
 			int randChoice = randObj.nextInt(4);
@@ -20,33 +19,8 @@ public class unknownSpot {
 			}
 		}
 		System.out.println("Score: " + points);
-		System.out.print((points/16.00)*100);
+		System.out.printf("%.2f", (points/17.00)*100);
 		System.out.println("%");
-		System.out.println("Credits: Designer: Methuki Laknara Wickramasinghe, Creator: Methsaan Dinakara Wickramasinghe, Dedicated to: Pamini Wickramasinghe, HAPPY B'DAY!");
-=======
-import java.util.Random;
-import java.util.Scanner;
-import java.util.Arrays;
-
-public class unknownSpot {
-	public static void main(String[] args) {
-		System.out.println("Welcome to Unknown Spot!");
-		int score = 0;
-		for (int x = 0; x < 12; x++) {
-			Random randObj = new Random();
-			int chosenNum = randObj.nextInt(6);
-			System.out.print("Enter a number between 1 and 6: ");
-			Scanner scanObj = new Scanner(System.in);
-			int usersNum = scanObj.nextInt();
-			if (chosenNum == usersNum) {
-				score += 1;
-				System.out.println("You earned treasure!");
-			}else {
-				System.out.println("No treasure here!");
-			}
-		}
-		System.out.println("Your score is " + Integer.toString(score));
-		System.out.println(Double.toString((score/12.00)*100.00)+"%");
->>>>>>> 11822cadafe468d24a2fad9515ddff261f943450
+		System.out.println("Credits: Designer: Methuki Laknara Wickramasinghe, Creator: Methsaan Dinakara Wickramasinghe");
 	}
 }
