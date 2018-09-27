05fdbf84d8beba3181b6ffa171027f88f68acc1b
Methsaan <methsaan@gmail.com>
2018-09-27 06:50:46 -0400

    update

--- games/nongraphical/hangman3.java
+++ games/nongraphical/hangman3.java
@@ -6,6 +6,14 @@ import java.util.Arrays;
 
 public class hangman3 {
 	public static void main(String[] args) {
+		System.out.print("LOADING ");
+		for (int X = 0; X < 10; X++){
+			System.out.print(".");
+			for (long Y = 0; Y < 20000000; Y++){
+				;
+			}
+		}
+		System.out.println();
 		Scanner scan = new Scanner(System.in);
 		System.out.print("Enter a word: ");
 		String word = scan.nextLine();
