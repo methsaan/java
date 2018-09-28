f3883ea94dba7e897d168d7292b42e234e030c5c
Methsaan <methsaan@gmail.com>
2018-09-28 19:01:25 -0400

    update

--- usefulprograms/divalg.java
+++ usefulprograms/divalg.java
@@ -2,9 +2,9 @@ import java.util.Scanner;
 
 public class divalg {
 	public static void main(String[] args){
-		System.out.print("Enter first number (2 digits): ");
+		System.out.print("Enter first number: ");
 		int dividend = new Scanner(System.in).nextInt();
-		System.out.print("Enter second number (2 digits): ");
+		System.out.print("Enter second number: ");
 		int divisor = new Scanner(System.in).nextInt();
 		double quotientDouble = (double)dividend/(double)divisor;
 		float quotientFloat = (float)(quotientDouble);
@@ -17,12 +17,9 @@ public class divalg {
 		System.out.println(divisor + " \t | " + dividTemp);
 		for (int x = 0; x < 7; x++){
 			numOfTimes = (int)dividTemp/divisor;
-			//System.out.println(dividTemp + " / " + divisor + " = " + numOfTimes);
 			ans = ans + Integer.toString(numOfTimes);
-			//System.out.println(divisor + " x " + numOfTimes + " = " + (divisor * numOfTimes));
 			System.out.println(" \t   " + (divisor*numOfTimes));
 			System.out.println(" \t   ___");
-			//System.out.println(dividTemp + " - " + (divisor*numOfTimes) + " = " + (dividTemp-divisor*numOfTimes));
 			if (dividTemp-divisor*numOfTimes > divisor) {
 				dividTemp = (dividTemp-divisor*numOfTimes);
 			}else {
@@ -30,6 +27,7 @@ public class divalg {
 			}
 			System.out.println(" \t   " + dividTemp);
 		}
+		//
 		float answer = Float.parseFloat(ans)/1000000;
 	}
 }
