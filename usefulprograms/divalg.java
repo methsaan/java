618fecb64e7d1d610e8f879fc35ec21893abe2c0
Methsaan <methsaan@gmail.com>
2018-09-29 09:16:29 -0400

    update

--- usefulprograms/divalg.java
+++ usefulprograms/divalg.java
@@ -6,6 +6,8 @@ public class divalg {
 		int dividend = new Scanner(System.in).nextInt();
 		System.out.print("Enter second number: ");
 		int divisor = new Scanner(System.in).nextInt();
+		System.out.print("Accuracy for irrational numbers: ");
+		int accuracy = new Scanner(System.in).nextInt();
 		double quotientDouble = (double)dividend/(double)divisor;
 		float quotientFloat = (float)(quotientDouble);
 		String ans = "";
@@ -13,10 +15,11 @@ public class divalg {
 		int dividTemp2;
 		int numOfTimes = 0;
 		int spaces = 3;
+		int dec = 0;
 		System.out.println(" \t    " + quotientFloat);
 		System.out.println(" \t " + "_________");
 		System.out.println(divisor + " \t | " + dividTemp);
-		for (int x = 0; x < 7; x++){
+		for (int x = 0;;x++) {
 			numOfTimes = (int)dividTemp/divisor;
 			ans = ans + Integer.toString(numOfTimes);
 			System.out.print(" \t");
@@ -32,6 +35,10 @@ public class divalg {
 			if (dividTemp-divisor*numOfTimes > divisor) {
 				dividTemp = (dividTemp-divisor*numOfTimes);
 			}else {
+				dec++;
+				if (dec == 1){
+					ans = ans + ".";
+				}
 				dividTemp = (dividTemp-divisor*numOfTimes) * 10;
 			}
 			System.out.print(" \t");
@@ -40,6 +47,14 @@ public class divalg {
 			}
 			System.out.println(dividTemp);
 			spaces++;
+			if (dividTemp == 0){
+				break;
+			}
+			if (x > accuracy){
+				System.out.println();
+				System.out.println("Irrational number. Approximate answer: " + ans);
+				break;
+			}
 		}
 	}
 }
