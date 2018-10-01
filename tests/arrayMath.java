918b4c2c1cf4268409e3a89ec0c876fb655ae6c1
Methsaan Wick@kali <methsaan@gmail.com>
2018-10-01 09:10:06 -0400

    update

--- tests/arrayMath.java
+++ tests/arrayMath.java
@@ -9,12 +9,18 @@ public class arrayMath {
 		Random randomChooser2;
 		int randNum2;
 		int sumArray;
+		System.out.print("mathArray: ");
+		for (int x = 0; x < mathArray.length; x++){
+			System.out.print(mathArray[x] + " ");
+		}
+		System.out.println();
 		for (int x = 0; x < 6; x++) {
 			randomChooser1 = new Random();
 			randNum1 = randomChooser1.nextInt(9) + 0;
 			randomChooser2 = new Random();
 			randNum2 = randomChooser2.nextInt(9) + 0;
 			sumArray = mathArray[randNum1] + mathArray[randNum2];
+			System.out.println("Adding " + mathArray[randNum1] + " and " + mathArray[randNum2]);
 			System.out.println("Adding random numbers from mathArray: " + sumArray);
 		}
 	}
