927bbccbe34c69ba62d1372a53c294619fdf7a02
Methsaan <methsaan@gmail.com>
2018-10-05 09:42:39 -0400

    update

--- tests/mainclass.java
+++ tests/mainclass.java
@@ -3,6 +3,6 @@ import java.util.*;
 public class mainclass{
 	public static void main(String[] args) {
 		int x = class1.input("Enter an integer: ");
-		class1.print("your number is", Integer.toString(x));
+		class1.print("your number is ", Integer.toString(x), " ", "32", " AAD");
 	}
 }
