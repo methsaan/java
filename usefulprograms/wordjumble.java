d62fc97b16cf5125a644a5e3725d791d13a22c0a
Methsaan <methsaan@gmail.com>
2018-12-01 19:37:38 -0500

    creating array for indexes

--- usefulprograms/wordjumble.java
+++ usefulprograms/wordjumble.java
@@ -7,23 +7,11 @@ public class wordjumble {
 		Random rand = new Random();
 		System.out.print("Enter a word: ");
 		String unjumbled = scan.nextLine();
-		int len = unjumbled.length();
-		int[] indexChoices = {0, 1, 2, 3, 4, 5};
-		int randomIndex = indexChoices[0];
 		String jumbled = "";
-		for (int x = 0; x < len; x++) {
-			while (randomIndex == len) {
-				randomIndex = indexChoices[rand.nextInt(6)];
-				System.out.print(randomIndex + " ");
-			}
-			System.out.println();
-			jumbled += unjumbled.substring(randomIndex, randomIndex+1);
-			for (int y = 0; y < len; y++){
-				if (indexChoices[x] == randomIndex){
-					indexChoices[x] = len;
-				}
-			}
+		int[] index = new = int[unjumbled.length()];
+		for (int x = 0; x < index.length; x++) {
+			index[x] = x;
 		}
-		System.out.println(jumbled);
+		int numOf
 	}
 }
