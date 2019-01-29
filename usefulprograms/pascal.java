cdcdedba0917cff62b445520787296abf958b8d6
Methsaan <methsaan@gmail.com>
2019-01-29 18:45:07 -0500

    adding the numbers

--- usefulprograms/pascal.java
+++ usefulprograms/pascal.java
@@ -1,25 +1,29 @@
 import java.util.ArrayList;
-import java.util.Scanner;
 
 public class pascal {
-	public static void main(String[] args){
-		Scanner input = new Scanner(System.in);
-		System.out.print("Enter number of rows: ");
-		int numberOfRows = input.nextInt();
-		int triangle[][] = new int[100][100];
-		triangle[0][0] = 0;
-		triangle[0][1] = 1;
-		triangle[0][2] = 0;
-		for (int x = 1; x < numberOfRows; x++){
+	public static void main(String[] argq){
+		int Triangle[][] = new int[100][14];
+		Triangle[0][0] = 1;
+		Triangle[1][0] = 1;
+		Triangle[1][1] = 1;
+		for (int x = 2; x < 14; x++) {
 			for (int y = 0; y < x; y++) {
-				triangle[x][y] = 1;
+				if ((y == 0) || (y == x-1)){
+					Triangle[x][y] = 1;
+				}else {
+					Triangle[x][y] = Triangle[x-1][y-1] + Triangle[x-1][y];
+				}
 			}
 		}
-		for (int x = 0; x < numberOfRows; x++){
+		for (int x = 0; x < 14; x++){
+			for (int i = 0; i < ((int)100/2-((x-1)*5))+8; i++) {
+				System.out.print(" ");
+			}
 			for (int y = 0; y < x; y++){
-				System.out.println(triangle[x][y]);
+				System.out.print(Triangle[x][y]);
+				System.out.print("       ");
 			}
-		}
-		
+			System.out.println();
+		}	
 	}
 }
