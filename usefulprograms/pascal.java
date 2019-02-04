4d52533ba9ddc01c8a540ab9b7d68336e18253be
Methsaan <methsaan@gmail.com>
2019-02-03 19:29:11 -0500

    aligning

--- usefulprograms/pascal.java
+++ usefulprograms/pascal.java
@@ -2,11 +2,11 @@ import java.util.ArrayList;
 
 public class pascal {
 	public static void main(String[] argq){
-		int Triangle[][] = new int[100][14];
+		int Triangle[][] = new int[100][15];
 		Triangle[0][0] = 1;
 		Triangle[1][0] = 1;
 		Triangle[1][1] = 1;
-		for (int x = 2; x < 14; x++) {
+		for (int x = 2; x < 15; x++) {
 			for (int y = 0; y < x; y++) {
 				if ((y == 0) || (y == x-1)){
 					Triangle[x][y] = 1;
@@ -15,13 +15,15 @@ public class pascal {
 				}
 			}
 		}
-		for (int x = 0; x < 14; x++){
-			for (int i = 0; i < ((int)100/2-((x-1)*5))+8; i++) {
+		for (int x = 0; x < 15; x++){
+			for (int i = 0; i < 90-x*5; i++) {
 				System.out.print(" ");
 			}
 			for (int y = 0; y < x; y++){
 				System.out.print(Triangle[x][y]);
-				System.out.print("       ");
+				for (int i = 0; i < 8; i++) {
+					System.out.print(" ");
+				}
 			}
 			System.out.println();
 		}	
