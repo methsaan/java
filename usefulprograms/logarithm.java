d1a7712696dafd7356923940d03251351f6bfc48
Methsaan <methsaan@gmail.com>
2019-02-06 06:59:38 -0500

    accepting floating point exponents

--- usefulprograms/logarithm.java
+++ usefulprograms/logarithm.java
@@ -4,16 +4,20 @@ public class logarithm {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the base: ");
-		int base = scan.nextInt();
+		double base = scan.nextDouble();
 		System.out.println("Enter the number: ");
-		int number = scan.nextInt();
+		double number = scan.nextDouble();
 		System.out.println("Log" + base + " " + number + " = " + log(base, number));
 	}
-	public static int log(int base, int number) {
-		int power = 1;
-		while (base != number) {
+	public static double log(double base, double number) {
+		double power = 0.1;
+		double diff = base-number;
+		if (diff < 0) {
+			diff = -diff;
+		}
+		while (diff > 0.1) {
 			number /= base;
-			power++;
+			power += 0.1;
 		}
 		return power;
 	}
