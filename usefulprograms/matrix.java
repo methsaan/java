354d6fbcc9be66fbabfa297eef1e0f5eef162003
Methsaan <methsaan@gmail.com>
2019-02-10 06:55:37 -0500

    choose formula using the pascal's triangle

--- usefulprograms/matrix.java
+++ usefulprograms/matrix.java
@@ -10,37 +10,38 @@ public class matrix {
 		input.nextLine();
 		System.out.print("Enter operator: ");
 		String operator = input.nextLine();
-		int matrix1[][] = new int[width1][height1];
+		int matrix1[][] = new int[height1][width1];
 		int width2 = operator.equals("x") ? height1 : width1;
 		int height2 = operator.equals("x") ? width1 : height1;
-		int matrix2[][] = new int[width2][height2];
-		System.out.println("Enter the values for matrix 1:");
-		for (int x = 0; x < height1; x++) {
-			for (int y = 0; y < width1; y++) {
+		int matrix2[][] = new int[height2][width2];
+		System.out.println("Enter values for matrix 1:");
+		for (int x = 0; x < matrix1.length; x++) {
+			for (int y = 0; y < matrix1[0].length; y++) {
 				matrix1[x][y] = input.nextInt();
 			}
 		}
-		System.out.println("Enter the values for matrix 2:");
-		for (int x = 0; x < height2; x++) {
-			for (int y = 0; y < width2; y++) {
+		System.out.println("Enter values for matrix 2:");
+		for (int x = 0; x < matrix2.length; x++) {
+			for (int y = 0; y < matrix2[0].length; y++) {
 				matrix2[x][y] = input.nextInt();
 			}
 		}
-		System.out.println("Matrix 1:");
-		for (int x = 0; x < height1; x++) {
-			System.out.print("[");
-			for (int y = 0; x < width1; y++) {
-				System.out.print(matrix1[x][y] + " ");
+		System.out.println();
+		for (int x = 0; x < matrix1.length; x++) {
+			for (int y = 0; y < matrix1[0].length; y++) {
+				System.out.print(matrix1[x][y] + " \t ");
 			}
-			System.out.println("]");
+			System.out.println();
+			System.out.println();
 		}
-		System.out.println("\nMatrix 2:");
-		for (int x = 0; x < height2; x++) {
-			System.out.print("[");
-			for (int y = 0; x < width2; y++) {
-				System.out.print(matrix2[x][y] + " ");
+		System.out.println();
+		System.out.println();
+		for (int x = 0; x < matrix2.length; x++) {
+			for (int y = 0; y < matrix2[0].length; y++) {
+				System.out.print(matrix2[x][y] + " \t ");
 			}
-			System.out.println("]");
+			System.out.println();
+			System.out.println();
 		}
 	}
 }	
