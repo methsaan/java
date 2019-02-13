dd7a9c12f61147119805a717c36474eed7ea121e
Methsaan <methsaan@gmail.com>
2019-02-13 10:32:31 -0500

    choose combination

--- usefulprograms/choose.java
+++ usefulprograms/choose.java
@@ -2,8 +2,9 @@ import java.util.Scanner;
 
 public class choose {
 	public static void main(String[] args) {
+		System.out.println(choose(12, 5));
 	}
-	public static int choose(int group, int )
-		C(n,r)=C(12,5)
-		=12!(5!(12−5)!)
+	public static int choose(int group, int combination) {
+		return group!(combination!(group-combination)!);
+	}
 }
