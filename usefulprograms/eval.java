55255debe63d562a321fab6f99dd97e5fc9829d0
Methsaan <methsaan@gmail.com>
2019-03-23 20:13:28 -0400

    accept command with spaces

--- usefulprograms/eval.java
+++ usefulprograms/eval.java
@@ -6,14 +6,14 @@ public class eval {
 		Scanner input = new Scanner(System.in);	
 		int lines = 10000;
 		String code = "";
-		String exit = "Object[] val = {eval_q()};";
+		String exit = "Object[] val = {q()};";
 		String[] declarations = new String[500];
-		int cnt = 0;
+		int deccnt = 0;
 		String[] statements = new String[20];
 		ProcessBuilder pb = new ProcessBuilder("./script");
 		pb.inheritIO();
 		pb.directory(new File("/home/test/programming/java/usefulprograms/"));
-		System.out.print("Java code evaluator.\nType \"eval_q()\" to  quit\nevalfile.java <code evaluator> # ");
+		System.out.print("Java code evaluator.\nType \"q()\" to  quit\nevalfile.java # ");
 		while (!code.equals(exit)) {
 			String value = input.nextLine();
 			if (!(value.contains("|"))) {
@@ -30,28 +30,33 @@ public class eval {
 					} while (first(numOfParen(code)) != last(numOfParen(code)) || numOfQuotes(code)%2 != 0);
 				}
 			}else {
-				//separate multiple statements
-				for (int x = 0; x < value.length()-1; x++) {
-					String tempstatement = "";
-					for (int y = 0; !value.substring(y, y+1).equals("|") && y < value.length()-1; y++) {
-						tempstatement += value.substring(y, y+1);
-					}
-					statements[x] = tempstatement;
-				}
-				//adds declarations to array
-				for (int x = 0; x < statements.length-1; x++) {
-					if (isDeclaration(statements[x])) {
-						declarations[cnt] = statements[x];
-						cnt++;
+				String temp = "";
+				int statementcnt = 0;
+				for (int x = 0; x < value.length(); x++) {
+					temp += value.substring(x, x+1);
+					if (value.substring(x, x+1).equals("|")) {
+						statements[statementcnt] = temp;
+						temp = "";
+						statementcnt++;
 					}
 				}
-				//combine statements to string
+				for (int x = 0; x < statementcnt; x++) {
+					if (isDeclaration(statements[x]) && numOfPipes(value) == 1) {
+						declarations[deccnt] = statements[x];
+						deccnt++;
+					}
+				}
 				code = "";
-				for (int x = 0; x < cnt; x++) {
-					code += declarations[x] + ";";
+				if (numOfPipes(value) == 1) {
+					for (int x = 0; x < deccnt; x++) {
+						code += pipeToSemi(declarations[x]);
+					}
+					if (!isDeclaration(value)) {
+						code += pipeToSemi(value);
+					}
+				}else {
+					code = pipeToSemi(value);
 				}
-				code += value + ";";
-				
 			}
 			BufferedWriter out = new BufferedWriter(new FileWriter("evalfile.java"));
 			ProcessBuilder shell = new ProcessBuilder();
@@ -59,10 +64,10 @@ public class eval {
 			out.write("import java.lang.*;\n"); 
 			out.write("import java.util.*;\n\n"); 
 			out.write("public class evalfile {;\n");
-			out.write("\tpublic static void main(String[] args) {\n");
+			out.write("\tpublic static void main(String[] args) throws IOException {\n");
 			out.write("\t\ttry {\n");
 			out.write("\t\t\t" + code + "\n");
-			if (!(value.contains("|"))) { 
+			if (!(value.contains("|"))) {
 				out.write("\t\t\tfor (int x = 0; x < val.length; x++) {\n");
 				out.write("\t\t\t\tSystem.out.println(val[x].toString());\n");
 				out.write("\t\t\t}\n");
@@ -71,18 +76,21 @@ public class eval {
 			out.write("\t\t\tSystem.out.println(e.getMessage());\n");
 			out.write("\t\t}\n");
 			out.write("\t}\n");
-			out.write("\tpublic static String eval_q() {\n");
-			out.write("\t\treturn \"Error: running outside of eval.java\";\n");
+			out.write("\tpublic static String q() {\n");
+			out.write("\t\treturn \"Error: running outside of eval\";\n");
 			out.write("\t}\n");
-			out.write("\tpublic static String eval_clear() {\n");
-			out.write("\t\treturn \"\033[H\033[2J\";\n");
+			out.write("\tpublic static void clear() {\n");
+			out.write("\t\tSystem.out.print(\"\\033[H\\033[2J\");\n");
 			out.write("\t}\n");
-			out.write("\tpublic static String eval_run(String x) throws IOException {\n");
-			out.write("\t\tProcessBuilder pb = new ProcessBuilder(x);\n");
+			out.write("\tpublic static void run(String x) throws IOException {\n");
+			out.write("\t\tBufferedWriter out = new BufferedWriter(new FileWriter(\"runscript\"));\n");
+			out.write("\t\tProcessBuilder shell = new ProcessBuilder();\n");
+			out.write("\t\tout.write(x);\n");
+			out.write("\t\tout.close();\n");
+			out.write("\t\tProcessBuilder pb = new ProcessBuilder(\"./runscript\");\n");
 			out.write("\t\tpb.inheritIO();\n");
 			out.write("\t\tpb.directory(new File(\"/home/test/programming/java/usefulprograms/\"));");
 			out.write("\t\tpb.start();\n");
-			out.write("\t\treturn \"eval_run()\";\n");
 			out.write("\t}\n");
 			out.write("}\n");
 			out.close();
@@ -90,6 +98,19 @@ public class eval {
 				pb.start();
 			}
 		}
+		ProcessBuilder pbquit = new ProcessBuilder("./compile");
+		pbquit.inheritIO();
+		pbquit.directory(new File("/home/test/programming/java/usefulprograms/"));
+		pbquit.start();
+	}
+	public static int numOfPipes(String str) {
+		int cnt = 0;
+		for (int x = 0; x < str.length(); x++) {
+			if (str.substring(x, x+1).equals("|")) {
+				cnt++;
+			}
+		}
+		return cnt;
 	}
 	public static String numOfParen(String str) {
 		int leftcnt = 0;
@@ -121,7 +142,7 @@ public class eval {
 	public static int numOfQuotes(String s) {
 		int cnt = 0;
 		for (int x = 0; x < s.length(); x++) {
-			if (s.substring(x, x+1).equals("\"")) {
+			if (s.substring(x, x+1).equals("|")) {
 				cnt++;
 			}
 		}
@@ -141,7 +162,7 @@ public class eval {
 	}
 	public static boolean containsType(String statement) {
 		boolean hasType = false;
-		String[] types = {"int ", "String ", "float ", "char ", "Scanner ", "Random ", "double ", "long ", "byte ", "short ", "int ", "int[] ", "String[] ","float[] ", "char[] ", "Scanner[] ", "Random[] ", "double[] ", "long[] ", "byte[] ", "short[] ", "int[] "};
+		String[] types = {"int ", "String ", "float ", "char ", "Scanner ", "Random ", "double ", "long ", "byte ", "short ", "Integer ", "Character ", "Double", "Float ", "int[] ", "String[] ","float[] ", "char[] ", "Scanner[] ", "Random[] ", "double[] ", "long[] ", "byte[] ", "short[] ", "Integer[] ", "Float[] ", "Double[] "};
 		for (int x = 0; x < types.length; x++) {
 			hasType = statement.contains(types[x]);
 			if (hasType) {
@@ -156,5 +177,16 @@ public class eval {
 		}else {
 			return false;
 		}
+	}
+	public static String pipeToSemi(String pipe) {
+		String semi = "";
+		for (int x = 0; x < pipe.length(); x++) {
+			if (pipe.substring(x, x+1).equals("|")) {
+				semi += ";";
+			}else {
+				semi += pipe.substring(x, x+1);
+			}
+		}
+		return semi;
 	}
 }
