5e383fbe143b0922013d797764492d22c146e3c2
Methsaan <methsaan@gmail.com>
2019-05-17 17:12:00 -0400

    substitute cone properties

---
+++ usefulprograms/cone.java
@@ -0,0 +1,15 @@
+public class cone {
+	public static double radius;
+	public static double height;
+	public static double area;
+	public static double volume;
+	public static double baseAngle;
+	public static double topAngle;
+	public static double radiusArea;
+	public static Object[] values = {"Radius", radius, "Height", height, "Area", area, "Volume", volume, "BaseAngle", baseAngle, "TopAngle", topAngle, "RadiusArea", radiusArea};
+	public static void fillInValues(String... values) {
+		
+	}
+	public static void update() {
+	}
+}
