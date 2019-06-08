1cc35689d4ddde6fc3f2f8fd52ff441a8087cb6b
Methsaan <methsaan@gmail.com>
2019-06-08 10:41:11 -0400

    distribute factor

--- usefulprograms/distributive.java
+++ usefulprograms/distributive.java
@@ -3,9 +3,24 @@ import java.util.Scanner;
 public class distributive {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter number of terms: ");
+		System.out.print("Enter number of terms: ");
 		term[] polynomial = new term[input.nextInt()];
-		System.out.println("Enter factor to distribute: ");
+		term[] distPolynomial = new term[polynomial.length];
+		System.out.print("Enter factor to distribute: ");
 		int factor = input.nextInt();
+		for (int x = 0; x < polynomial.length; x++) {
+			System.out.print("Enter coefficient: ");
+			polynomial[x].setCoefficient(input.nextInt());
+			input.nextLine();
+			System.out.print("Enter variable part: ");
+			polynomial[x].setVariable(input.nextLine());
+		}
+		for (int x = 0; x < distPolynomial.length; x++) {
+			distPolynomial[x].setCoefficient(polynomial[x].coefficient()*factor);
+		}
+		for (int x = 0; x < polynomial.length; x++) {
+			System.out.print(polynomial[x].variable() + " ");
+		}
+		System.out.println();
 	}
 }
--- usefulprograms/term.java
+++ usefulprograms/term.java
@@ -1,7 +1,7 @@
 public class term {
-	private int coefficient;
-	private String variable;
-	private String polarity;
+	private static int coefficient;
+	private static String variable;
+	private static String polarity;
 	public static int coefficient() {
 		return coefficient;
 	}
@@ -13,6 +13,7 @@ public class term {
 	}
 	public static void setCoefficient(int num) {
 		coefficient = num;
+		polarity = coefficient < 0 ? "-" : "+";
 	}
 	public static void setVariable(String str) {
 		variable = str;
