fc677a935d603bffa5a74e429d79149e89be2b91
Methsaan <methsaan@gmail.com>
2019-06-18 17:54:30 -0400

    add polarity to original coefficient

--- usefulprograms/distributive.java
+++ usefulprograms/distributive.java
@@ -11,19 +11,23 @@ public class distributive {
 		for (int x = 0; x < polynomial.length; x++) {
 			System.out.print("Enter coefficient: ");
 			polynomial[x].setCoefficient(input.nextInt());
+			polynomial[x].setPolarity(polynomial[x].coefficient() < 0 ? "-" : "+");
 			input.nextLine();
 			System.out.print("Enter variable part: ");
 			polynomial[x].setVariable(input.nextLine());
+			System.out.println(polynomial[x].polarity() + Math.abs(polynomial[x].coefficient()) + polynomial[x].variable());
 		}
-		for (int x = 0; x < polynomial.length; x++) {
-			System.out.print(polynomial[x].polarity() + Math.abs(distPolynomial[x].coefficient()) + distPolynomial[x].variable());
+		for (int i = 1; i < 2; i++) {
+			System.out.println(polynomial[i].polarity() + Math.abs(polynomial[i].coefficient()) + polynomial[i].variable());
+		}
+		System.out.println();
+		for (int x = 0; x < distPolynomial.length; x++) {
+			distPolynomial[x].setPolarity(polynomial[x].coefficient()*factor < 0 ? "-" : "+");
+			distPolynomial[x].setCoefficient(polynomial[x].coefficient()*factor);
+			distPolynomial[x].setVariable(polynomial[x].variable());
 		}
 		for (int x = 0; x < distPolynomial.length; x++) {
-			distPolynomial[x].setCoefficient(polynomial[x].coefficient);
-			distPolynomial[x].setVariable(polynomial[x].variable);
-		}
-		for (int x = 0; x < distPolynomial.length; x++) {
-			System.out.print(distPolynomial[x].polarity() + Math.abs(distPolynomial[x].coefficient()) + distPolynomial[x].variable());
+			System.out.print(distPolynomial[x].polarity() + Math.abs(distPolynomial[x].coefficient()) + distPolynomial[x].variable() + " ");
 		}
 		System.out.println();
 	}
