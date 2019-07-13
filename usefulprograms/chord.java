c0d0dea7ba4da50964e395f98e0e3ef701e22c68
Methsaan <methsaan@gmail.com>
2019-07-13 11:04:49 -0400

    notes of a scale

--- usefulprograms/chord.java
+++ usefulprograms/chord.java
@@ -4,7 +4,6 @@ import java.util.Arrays;
 public class chord {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		String[] notes = {"C", "C#", "D", "D#", "E", "F", "F#", "G", "G#", "A", "A#", "B", "C'", "C#'", "D'", "D#'", "E'", "F'", "F#'", "G'", "G#'", "A'", "A#'", "B'"};
 		int[] major = {0, 2, 4, 5, 7, 9, 11, 12};
 		int[] minorHarmonic = {0, 2, 3, 5, 7, 8, 11, 12};
 		int[] minorMelodic = {0, 2, 3, 5, 7, 9, 11, 12};
@@ -53,6 +52,24 @@ public class chord {
 		}else {
 			chosenScale = major.clone();
 		}
+		String[] flatMajKeys = {"F", "Bb", "Eb", "Ab", "Db", "Gb"};
+		String[] flatMinKeys = {"D", "G", "C", "F", "Bb", "Eb"};
+		boolean majCondition = scaleChord.contains("Major") && isIn(note, flatMajKeys);
+		boolean minCondition = scaleChord.contains("Minor") && isIn(note, flatMinKeys);
+		System.out.println(note);
+		System.out.println("Major flat key: " + majCondition);
+		System.out.println("Minor flat key: " + minCondition);
+		String a = (majCondition || minCondition) ? "Db" : "C#";
+		String b = (majCondition || minCondition) ? "Eb" : "D#";
+		String c = (majCondition || minCondition) ? "Gb" : "F#";
+		String d = (majCondition || minCondition) ? "Ab" : "G#";
+		String e = (majCondition || minCondition) ? "Bb" : "A#";
+		String f = (majCondition || minCondition) ? "Db'" : "C#'";
+		String g = (majCondition || minCondition) ? "Eb'" : "D#'";
+		String h = (majCondition || minCondition) ? "Gb'" : "F#'";
+		String i = (majCondition || minCondition) ? "Ab'" : "G#'";
+		String j = (majCondition || minCondition) ? "Bb'" : "A#'";
+		String[] notes = {"C", a, "D", b, "E", "F", c, "G", d, "A", e, "B", "C'", f, "D'", g, "E'", "F'", h, "G'", i, "A'", j, "B'"};
 		for (int x = 0; x < chosenScale.length; x++) {
 			System.out.print(notes[chosenScale[x]+findIndex(notes, note)] + " ");
 		}
@@ -65,5 +82,13 @@ public class chord {
 			}
 		}
 		return 0;
+	}
+	public static boolean isIn(String str, String[] arr) {
+		for (int x = 0; x < arr.length; x++) {
+			if (arr[x].equals(str)) {
+				return true;
+			}
+		}
+		return false;
 	}
 }
