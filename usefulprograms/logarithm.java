8ffc382137889f1d3c5f24dcde50b401ef4a3ed3
Methsaan <methsaan@gmail.com>
2019-07-13 20:57:34 -0400

    round digits

--- usefulprograms/logarithm.java
+++ usefulprograms/logarithm.java
@@ -1,20 +1,34 @@
 import java.util.Scanner;
+import java.text.*;
 
 public class logarithm {
 	public static void main(String[] args) {
+		DecimalFormat decFor;
 		Scanner scan = new Scanner(System.in);
 		System.out.println("Enter the base: ");
 		double base = scan.nextDouble();
 		System.out.println("Enter the number: ");
 		double number = scan.nextDouble();
-		System.out.printf("Log%f   %f   =   %f\n", base, number, log(base, number));
+		System.out.print("Log");
+		decFor = new DecimalFormat("0." + strmult(Double.toString(base), numOfDigits(base)));
+		System.out.print(decFor.format(base) + "  ");
+		decFor = new DecimalFormat("0." + strmult(Double.toString(number), numOfDigits(number)));
+		System.out.print(decFor.format(number) + "  =  ");
+		decFor = new DecimalFormat("0." + strmult(Double.toString(log(base, number)), numOfDigits(log(base, number))));
+		System.out.println(decFor.format(log(base, number)));
 	}
 	public static double log(double base, double number) {
-		double power = 0.000001;
-		while (base != number) {
-			number /= base;
-			power++;
+		return Math.log10(number)/Math.log10(base);
+	}
+	public static int numOfDigits(double num) {
+		int decIndex = Double.toString(num).indexOf(".");
+		return Double.toString(num).substring(decIndex, Double.toString(num).length());
+	}
+	public static String strmult(String str, int numOfTimes) {
+		String multstr;
+		for (int x = 0; x < numOfTimes; x++) {
+			multstr += str;
 		}
-		return power;
+		return multstr;
 	}
 }
