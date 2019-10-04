c707ca1220e40cfdd10679208d33d7f08c2fe563
Methsaan <methsaan@gmail.com>
2019-10-03 21:32:52 -0400

    store composite factors

--- usefulprograms/primeFactors.java
+++ usefulprograms/primeFactors.java
@@ -1,21 +1,30 @@
 import java.util.Scanner;
 import java.util.ArrayList;
+import java.util.Collections;
 
 public class primeFactors {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
 		System.out.println("Enter a composite number: ");
 		int number = input.nextInt();
-		ArrayList<Integer> primeFactors = new ArrayList<Integer>();
-		ArrayList<Integer> tempFactors = new ArrayList<Integer>();
-		ArrayList<Integer> factors = factors(number);
-		System.out.println(factors);
-		for (int x = 0; x < 1; x++) {
-			tempFactors.add(factors.get(0));
-			tempFactors.add(factors.get(factors.size()-1));
-			System.out.println(tempFactors.get(0));
-			System.out.println(tempFactors.get(1));
+		ArrayList<ArrayList<Integer>> factorTree = new ArrayList<ArrayList<Integer>>();
+		ArrayList<Integer> tempFactors = factors(number);
+		while (true) {
+			System.out.println(tempFactors);
+			tempFactors = compFactors(tempFactors);
+			if (tempFactors.size() == 0) {
+				break;
+			}
 		}
+	}
+	public static ArrayList<Integer> compFactors(ArrayList<Integer> factorsList) {
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for (int x = 0; x < factorsList.size(); x++) {
+			if (isPrime(factorsList.get(x).intValue())) {
+				result.add(factorsList.get(x));
+			}
+		}
+		return result;
 	}
 	public static ArrayList<Integer> factors(int num) {
 		ArrayList<Integer> f = new ArrayList<Integer>();
@@ -25,6 +34,7 @@ public class primeFactors {
 				f.add(cnt, new Integer(x));
 			}
 		}
+		Collections.reverse(f);
 		return f;
 	}
 	public static void printList(ArrayList<Integer> arr) {
