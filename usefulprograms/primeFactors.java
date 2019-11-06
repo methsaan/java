ae08e5584402af530767ade8260586c264ab548e
Methsaan <methsaan@gmail.com>
2019-11-05 20:32:21 -0500

    update

--- usefulprograms/primeFactors.java
+++ usefulprograms/primeFactors.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Random;
@@ -6,7 +7,7 @@ import java.util.Random;
 public class primeFactors {
 	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		System.out.println("Enter a composite number: ");
+		System.out.print("Enter a composite number: ");
 		int number = input.nextInt();
 		ArrayList<ArrayList<Integer>> factorTree = new ArrayList<ArrayList<Integer>>();
 		ArrayList<Integer> tempRow = new ArrayList<Integer>();
@@ -18,13 +19,16 @@ public class primeFactors {
 			ArrayList<Integer> currentRow = factorTree.get(factorTree.size()-1);
 			ArrayList<Integer> nextRow = new ArrayList<Integer>();
 			for (int x = 0; x < currentRow.size(); x++) {
-				if (!isPrime(currentRow.get(x))) {
-					int randFactor = factors(currentRow.get(x)).get(new Random().nextInt(factors(currentRow.get(x)).size()-1));
-					nextRow.add(randFactor);
-					nextRow.add(currentRow.get(x)/randFactor);
-				}else {
-					primeFactors.add(currentRow.get(x));
-					System.out.println(currentRow.get(x));
+				if (currentRow.get(x) != -1) {
+					if (!isPrime(currentRow.get(x))) {
+						int randFactor = factors(currentRow.get(x)).get(new Random().nextInt(factors(currentRow.get(x)).size()-1));
+						nextRow.add(randFactor);
+						nextRow.add(currentRow.get(x)/randFactor);
+					}else {
+						nextRow.add(-1);
+						nextRow.add(-1);
+						primeFactors.add(currentRow.get(x));
+					}
 				}
 			}
 			factorTree.add(nextRow);
@@ -35,12 +39,24 @@ public class primeFactors {
 				}
 			}
 		}
+		for (int x = 0; x < factorTree.get(factorTree.size()-1).size(); x++) {
+			if (isPrime(factorTree.get(factorTree.size()-1).get(x))) {
+				primeFactors.add(factorTree.get(factorTree.size()-1).get(x));
+			}
+		}
 		System.out.println();
 		for (int x = 0; x < factorTree.size(); x++) {
 			System.out.println(factorTree.get(x));
 		}
 		System.out.print("Prime factors: ");
-		System.out.println(primeFactors);
+		System.out.println("\t" + primeFactors);
+		int[] arraypf = primeFactors.stream().filter(t -> t != null).mapToInt(t -> t).toArray();
+		int[] leastToGreatest = sortInts(arraypf);
+		ArrayList<Integer> ltogArraylist = new ArrayList<Integer>();
+		for (int x = 0; x < leastToGreatest.length; x++) {
+			ltogArraylist.add(leastToGreatest[x]);
+		}
+		System.out.println(" =\t\t" + ltogArraylist);
 	}
 	public static ArrayList<Integer> factors(int num) {
 		ArrayList<Integer> f = new ArrayList<Integer>();
@@ -61,5 +77,25 @@ public class primeFactors {
 	}
 	public static boolean isPrime(int num) {
 		return factors(num).size()+1 == 2;
+	}
+	public static int []sortInts(int[] asd) {
+		int[] sorted = asd.clone();
+		for (int i = 0; i < sorted.length; i++) {
+			for (int j = i+1; j < sorted.length; j++) {
+				if ( (sorted[i] > sorted[j]) && (i != j) ) {
+					int temp = sorted[j];
+					sorted[j] = sorted[i];
+					sorted[i] = temp;
+				}
+			}
+		}
+		return sorted;
+	} 
+	public static int[] convertIntegers(ArrayList<Integer> integers) {
+		int[] ret = new int[integers.size()];
+		for (int i=0; i < ret.length; i++) {
+			ret[i] = integers.get(i).intValue();
+		}
+		return ret;
 	}
 }
