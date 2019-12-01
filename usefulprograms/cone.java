5c4cc4aaa79ab4e71c43dcf3af814352a4e79b91
Methsaan <methsaan@gmail.com>
2019-12-01 14:04:32 -0500

    cone methods

--- usefulprograms/cone.java
+++ usefulprograms/cone.java
@@ -3,7 +3,5 @@ public class cone {
 	public static double height;
 	public static double area;
 	public static double volume;
-	public static double baseAngle;
-	public static double topAngle;
-	public static double radiusArea;
+	public static String area
 }
--- usefulprograms/coneMain.java
+++ usefulprograms/coneMain.java
@@ -1,4 +1,17 @@
+import java.util.*;
+
 public class coneMain {
 	public static void main(String[] args) {
+		cone c = new cone();
+		Scanner input = new Scanner(System.in);
+		String[] values = new String[4];
+		Map measurementVal = new Hashtable();
+		System.out.println("Enter cone measurements (radius, height, area, volume) ( _ for missing): ");
+		values = input.nextLine().split(" ");
+		for (int x = 0; x < 4; x++) {
+			String[] valueNames = {"radius", "height", "area", "volume"};
+			measurementVal.put(valueNames[x], values[x]);
+		}
+		System.out.println(measurementVal);
 	}
 }
