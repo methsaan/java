3682fedb53130c07f33016174250acfdfccfd400
Methsaan <methsaan@gmail.com>
2019-12-06 15:47:45 -0500

    filling in missing values

--- usefulprograms/cone.java
+++ usefulprograms/cone.java
@@ -3,5 +3,13 @@ public class cone {
 	public static double height;
 	public static double area;
 	public static double volume;
-	public static String area
+	public cone(String r, String h, String a, String v) {
+		radius = r != "_" ? r : -1;
+		height = h != "_" ? h : -1;
+		area = a != "_" ? a : -1;
+		volume = v != "_" ? v : -1;
+	}
+	public static String area() {
+		return new coneMethod({"radius", "height"}, ((2*Math.pi)*r*h)/3);
+	}
 }
