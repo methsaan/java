937b50afaeed42a28f70b7bed40beb933c6c3c67
Methsaan <methsaan@gmail.com>
2020-03-24 11:52:47 -0400

    z

--- usefulprograms/addalg.java
+++ usefulprograms/addalg.java
@@ -16,19 +16,19 @@ public class addalg {
 		int rem2;
 		if ((f > 0) && (s > 0) && (t > 0) && (fo > 0)) {
 			rem1 = Integer.parseInt(Integer.toString(Integer.parseInt(Integer.toString(s).substring(1)) + Integer.parseInt(Integer.toString(f).substring(1)) + Integer.parseInt(Integer.toString(t).substring(1)) + Integer.parseInt(Integer.toString(fo).substring(1))));
-			rem2 = Integer.parseInt(Integer.toString(rem1).substring(0, 1));
+			rem2 = Integer.toString(rem1).length() != 1 ? Integer.parseInt(Integer.toString(rem1).substring(0, 1)) : 0;
 		}else if ((f > 0) && (s > 0) && (t > 0)) {
 			rem1 = Integer.parseInt(Integer.toString(Integer.parseInt(Integer.toString(s).substring(1)) + Integer.parseInt(Integer.toString(f).substring(1)) + Integer.parseInt(Integer.toString(Integer.parseInt(Integer.toString(t))))));
-			rem2 = Integer.parseInt(Integer.toString(rem1).substring(0, 1));
+			rem2 = Integer.toString(rem1).length() != 1 ? Integer.parseInt(Integer.toString(rem1).substring(0, 1)) : 0;
 		}else {
 			rem1 = Integer.parseInt(Integer.toString(Integer.parseInt(Integer.toString(s).substring(1)) + Integer.parseInt(Integer.toString(f).substring(1))));
-			rem2 = Integer.parseInt(Integer.toString(rem1).substring(0, 1));
+			rem2 = Integer.toString(rem1).length() != 1 ? Integer.parseInt(Integer.toString(rem1).substring(0, 1)) : 0;
 		}
-		System.out.println("   " + rem2);
+		System.out.print(rem2 != 0 ? ("   " + rem2 + "\n") : "");
 		System.out.println("   " + f);
 		System.out.println("   " + s);
-		System.out.println(t != 0 ? "   " + t : "    " + t);
-		System.out.println(fo != 0 ? "   " + fo : "    " + fo);
+		System.out.println((t != 0 ? "   " : "    ") + t);
+		System.out.println((fo != 0 ? "   " : "    ") + fo);
 		System.out.println(" +____");
 		System.out.println((Integer.toString(f+s+t+fo).length() != 2 ? "  " : "   ") + (f+s+t+fo));
 	}
--- usefulprograms/caesercipher.java
+++ usefulprograms/caesercipher.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class caesercipher {
 	public static void main(String[] args) {
-		char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y'};
+		char[] letters = {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 		Scanner input = new Scanner(System.in);
 		System.out.print("Enter word to encrypt: ");
 		String word = input.nextLine();
