0019bc21c8abc5bc38e3b04e1bf80008552d4de1
Methsaan <methsaan@gmail.com>
2020-08-09 19:26:21 -0400

    argument error

--- usefulprograms/cone.java
+++ usefulprograms/cone.java
@@ -5,47 +5,56 @@ public class cone {
 	public static double height;
 	public static double area;
 	public static double volume;
+	private static int valuesSet;
 	public cone(String r, String h, String a, String v) {
 		radius = !r.equals("_") ? Integer.parseInt(r) : -1;
 		height = !h.equals("_") ? Integer.parseInt(h) : -1;
 		area = !a.equals("_") ? Integer.parseInt(a) : -1;
 		volume = !v.equals("_") ? Integer.parseInt(v) : -1;
 	}
+	public void setValuesSet(int valuesSet) {
+		this.valuesSet = valuesSet;
+	}
+	public static int getValuesSet() {
+		return valuesSet;
+	}
 	public static void fillVolume() {
-		System.out.println("Calculating volume (given radius, height)");
 		if (radius != -1 && height != -1) {
 			volume = Math.PI*Math.pow(radius, 2)*(height/3);
-			System.out.printf("PI*(%f^2)*(%f/3)\n", radius, height);
 		}
 	}
 	public static void fillArea() {
-		System.out.println("Calculating area (given radius, height)");
 		if (radius != -1 && height != -1) {
 			area = (Math.PI*Math.pow(radius, 2)) + (Math.PI*radius*(Math.sqrt(Math.pow(radius, 2) + Math.pow(height, 2))));
-			System.out.printf("(PI*%f^2) + (PI*%f*(sqrt(%f^2 + %f^2)))\n", radius, radius, radius, height);
 		}
 	}
 	public static void fillHeight() {
-		System.out.println("Calculating height (given radius, volume)");
 		if (radius != -1 && volume != -1) {
 			height = (volume*3)/Math.pow(radius, 2)/Math.PI;
-			System.out.printf("(%f*3)/%f^2/PI\n", volume, radius);
 		}
 	}
 	public static void fillRadius() {
-		System.out.println("Calculating radius (given height, volume)");
 		if (height != -1 && volume != -1) {
 			radius = ((volume*3)/height)/(2*Math.PI);
-			System.out.printf("((%d*3)/%d)/(2*PI)\n", volume, height);
 		}
 	}
 	public static void printValues() {
 		System.out.println("radius: " + radius + ", volume: " + volume + ", height: " + height + ", area: " + area);
 	}
-	public static boolean fullInit() {
+	public boolean fullInit() {
 		if (radius != -1.0 && height != -1.0 && area != -1.0 && volume != -1.0) {
+			return true;
+		} else if (this.getValuesSet() == 2) {
+			System.out.printf("Invalid arguments: Cannot calculate %s from %s\n", printList(radius != -1 ? "" : "radius", height != -1 ? "" : "height", area != -1 ? "" : "area", volume != -1 ? "" : "volume"), printList(radius == -1 ? "" : "radius", height == -1 ? "" : "height", area == -1 ? "" : "area", volume == -1 ? "" : "volume"));
 			return true;
 		}
 		return false;
+	}
+	private static String printList(String... items) {
+		String result = "";
+		for (int x = 0; x < items.length; x++) {
+			result += x == items.length-1 ? ("and " + items[x]) : ((items[x] == "" || x == items.length-2) ? items[x] : (items[x] + ", "));
+		}
+		return result;
 	}
 }
