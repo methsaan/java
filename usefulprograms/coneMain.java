7fd73754ffbc1dfad75034fa8bea94ee42aaf6d6
Methsaan <methsaan@gmail.com>
2020-08-09 19:26:21 -0400

    argument error

--- usefulprograms/coneMain.java
+++ usefulprograms/coneMain.java
@@ -13,30 +13,12 @@ public class coneMain {
 		}
 		System.out.println(measurementVal);
 		cone c = new cone(String.valueOf(measurementVal.get("radius")), String.valueOf(measurementVal.get("height")), String.valueOf(measurementVal.get("area")), String.valueOf(measurementVal.get("volume")));
-		c.printValues();
-		for (int x = 0; x < 2; x++) /*while (!c.fullInit())*/ {
-			System.out.println();
-			System.out.println("--------------------------------------------");
+		while (!c.fullInit()) {
 			c.fillRadius();
-			c.printValues();
-			System.out.println("--------------------------------------------");
-			System.out.println();
-			System.out.println("--------------------------------------------");
 			c.fillHeight();
-			c.printValues();
-			System.out.println("--------------------------------------------");
-			System.out.println();
-			System.out.println("--------------------------------------------");
 			c.fillVolume();
-			c.printValues();
-			System.out.println("--------------------------------------------");
-			System.out.println();
-			System.out.println("--------------------------------------------");
 			c.fillArea();
-			c.printValues();
-			System.out.println("--------------------------------------------");
-			System.out.println();
-			// add more arguments
+			c.setValuesSet(c.getValuesSet()+1);
 		}
 		c.printValues();
 	}
