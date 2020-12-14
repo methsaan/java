b3ded26d0703df048351ee586386fa9c1253fd68
Methsaan <methsaan@gmail.com>
2020-12-14 06:43:28 -0500

    creating command file

--- usefulprograms/eval.java
+++ usefulprograms/eval.java
@@ -1,6 +1,18 @@
 import java.util.Scanner;
+import java.io.FileWriter;
+import java.io.IOException;
 
 public class eval {
 	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		FileWriter commandFile = new FileWriter("evalrun.java");
+		String command;
+		while (command.equals("" /*QUIT COMMAND*/ )) {
+			System.out.print("Enter command: ");
+			command = input.nextLine();
+			// write command to file
+			// all variables global
+		}
+		commandFile.close();
 	}
 }
