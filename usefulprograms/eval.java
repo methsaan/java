532ee48a3c6f983c7e1fd54a96ccf655efdf7761
Methsaan <methsaan@gmail.com>
2021-02-08 13:00:46 -0500

    file open error handle

--- usefulprograms/eval.java
+++ usefulprograms/eval.java
@@ -4,15 +4,22 @@ import java.io.IOException;
 
 public class eval {
 	public static void main(String[] args) {
-		Scanner input = new Scanner(System.in);
-		FileWriter commandFile = new FileWriter("evalrun.java");
-		String command;
-		while (command.equals("" /*QUIT COMMAND*/ )) {
-			System.out.print("Enter command: ");
-			command = input.nextLine();
-			// write command to file
-			// all variables global
+		try {
+			Scanner input = new Scanner(System.in);
+			String command = "";
+			while (!command.equals("return 0; // FINISHED")) {
+				FileWriter commandFile = new FileWriter("evalrun.java");
+				command = "";
+				while (!command.startsWith("return 0;")) {
+					System.out.print("Enter command: ");
+					command = input.nextLine();
+					commandFile.write(command + System.getProperty("line.separator"));
+				}
+				commandFile.close();
+				System.out.println("HAHO");
+			}
+		} catch (Exception e) {
+			System.out.println(e);
 		}
-		commandFile.close();
 	}
 }
