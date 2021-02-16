64baa7079b74c54bbea4d97736c5e44c21ddb876
Methsaan <methsaan@gmail.com>
2021-02-15 22:19:01 -0500

    read file

--- usefulprograms/eval.java
+++ usefulprograms/eval.java
@@ -1,9 +1,11 @@
 import java.util.Scanner;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 
 public class eval {
 	public static void main(String[] args) {
+		ProcessBuilder processBuilder = new ProcessBuilder();
+		processBuilder.command("./hello.sh");
+
 		try {
 			Scanner input = new Scanner(System.in);
 			String command = "";
@@ -16,9 +18,21 @@ public class eval {
 					commandFile.write(command + System.getProperty("line.separator"));
 				}
 				commandFile.close();
-				System.out.println("HAHO");
+				Process process = processBuilder.start();
+				StringBuilder output = new StringBuilder();
+				BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
+				String line;
+				while ((line = reader.readLine()) != null) {
+					output.append(line + "\n");
+				}
+				int exitVal = process.waitFor();
+				if (exitVal == 0) {
+					System.out.println(output);
+				}else {
+					System.out.println("Error");
+				}
 			}
-		} catch (Exception e) {
+		}catch (Exception e) {
 			System.out.println(e);
 		}
 	}
